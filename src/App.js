d0824a78f7d8a5d9de930f9fc79479debd3d8c1c
mahderetewelign@gmail.com <mahderetewelign@gmail.com>
2022-07-03 15:56:48 +0300

    footer

--- src/App.js
+++ src/App.js
@@ -6,6 +6,11 @@ import React from "react";
 import Header from "./components/Header";
 import OptionsTab from "./components/OptionsTab";
 import LocationCards from "./components/LocationCards";
+import Footer from "./components/Footer";
+import FooterMenu from "./components/FooterMenu";
+
+import { displayOnDesktop } from "./themes/commonStyles";
+import MobileFooter from "./components/MobileFooter";
 
 function App() {
   return (
@@ -21,9 +26,32 @@ function App() {
         <Box>
           <Header />
           <OptionsTab />
-          <Container maxWidth="xl" sx={{ mb: 3 }}>
-            <LocationCards />
-          </Container>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              flexGrow: 1,
+              height: "75vh",
+              overflowY: "scroll",
+            }}
+          >
+            <Container maxWidth="xl" sx={{ mb: 3 }}>
+              <LocationCards />
+              <Box
+                sx={{
+                  display: { xs: "flex", md: "none" },
+                }}
+              >
+                <MobileFooter />
+              </Box>
+            </Container>
+          </Box>
+        </Box>
+        <Box sx={{ display: { xs: "flex", md: "none" } }}>
+          <FooterMenu />
+        </Box>
+        <Box sx={displayOnDesktop}>
+          <Footer />
         </Box>
       </Box>
     </React.Fragment>
